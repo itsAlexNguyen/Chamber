1c639fc5daf948ad1196c3b46e0603cac3d3f9d5
Alex Nguyen <alex.nguyen@td.com>
2017-07-23 15:57:31 -0400

    Initial Commit

---
+++ Chamber/Chamber.h
@@ -0,0 +1,19 @@
+//
+//  Chamber.h
+//  Chamber
+//
+//  Created by Alex Nguyen on 2017-07-23.
+//  Copyright © 2017 Gopher Labs LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Chamber.
+FOUNDATION_EXPORT double ChamberVersionNumber;
+
+//! Project version string for Chamber.
+FOUNDATION_EXPORT const unsigned char ChamberVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Chamber/PublicHeader.h>
+
+
